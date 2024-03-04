d461fbe7324f01babce3e59fe152170c97d84a17
AldyDPP <possiblepiano@gmail.com>
2024-03-04 23:19:17 +0700

    fix: avoidbots wrong function name

--- bot_starterpack/game/logic/naive_avoidbots.py
+++ bot_starterpack/game/logic/naive_avoidbots.py
@@ -15,31 +15,49 @@ class NaiveAvoidBots(BaseLogic) :
         x,y = board_bot.position.x, board_bot.position.y
         bots,diamonds = board.bots,board.diamonds
         bx,by = board_bot.properties.base.x, board_bot.properties.base.y
-        dx,dy = board_bot.properties.base.x - board_bot.position.x, board_bot.properties.base.y - board_bot.position.y
         bots = [b for b in bots if (b.id != board_bot.id)]
 
         print("Diamonds: ", board_bot.properties.diamonds)
+        print("Distance from bots: ")
+        for b in bots :
+            print(f"bot-{b.id}: ", dist((b.position.x,b.position.y), (x,y)))
+
 
         # If home is really close (and I have diamonds), go home!
-        if (dist((bx,by),(x,y)) <= 1) and board_bot.properties.diamonds > 0:
-            print("Force go home, dist = ", dist((dx,dy),(x,y)))
+        if (dist((bx,by), (x,y)) <= 1) and (board_bot.properties.diamonds > 0):
+            dx, dy = board_bot.properties.base.x - board_bot.position.x, board_bot.properties.base.y - board_bot.position.y
+            print("Force go home, dist = ", dist((bx,by),(x,y)))
             return self.movedir(dx,dy)
 
         # Bot avoid guard
         pa,pb = 10**9,10**9
         for b in bots :
-            if dist((pa,pb),(x,y)) > (dist((b.position.x,b.position.y),(x,y))):
-                pa,pb = (b.position.x,b.position.y)
+            if dist((pa,pb),(x,y)) > dist((b.position.x,b.position.y),(x,y)):
+                pa, pb = b.position.x, b.position.y
         if dist((pa,pb),(x,y)) <= 3 :
-            print("Evade from:", pa,pb)
-            dx,dy = pa - x, pb - y
-            return self.movdir(-dx,-dy)
+            print("Evade from: ", pa, pb)
+            dx,dy = pa - x, pb - y       
+            dx,dy = self.movedir(-dx, -dy)
+            # Suren
+            if x + dx >= 15 :
+                return (-1,0)
+            elif x + dx < 0 :
+                return (1,0)
+            elif y + dy >= 15 :
+                return (0,-1)
+            elif y + dy < 0 :
+                return (0,1)
+            else :
+                return (dx,dy)
+        
         # Return home
         if board_bot.properties.diamonds + 1 >= board_bot.properties.inventory_size:
+            print("Going home...")
             dx,dy = board_bot.properties.base.x - board_bot.position.x, board_bot.properties.base.y - board_bot.position.y
             return self.movedir(dx,dy)
 
         else:
+            print("Finding nearest diamond...")
             nearest = min(diamonds, key=lambda d : dist((d.position.x,d.position.y),(x,y)))
             dx,dy = nearest.position.x - x, nearest.position.y - y
             return self.movedir(dx,dy)
--- bot_starterpack/run-bots.bat
+++ bot_starterpack/run-bots.bat
@@ -1,5 +1,7 @@
 @echo off
-start cmd /c "python main.py --logic n_ab --email=ALDY@email.com --name=aldy_1 --password=123456 --team etimo"
-start cmd /c "python main.py --logic RAFLY --email=RAFLY@email.com --name=rafly_1 --password=123456 --team etimo"
-start cmd /c "python main.py --logic n_ab --email=ALDY2@email.com --name=aldy_2 --password=123456 --team etimo"
-start cmd /c "python main.py --logic RAFLY --email=RAFLY2@email.com --name=rafly_2 --password=123456 --team etimo"
+start cmd /c "python main.py --logic n_ab --email=ya@example.com --name=al1 --password=12312423412512341234 --team etimo"
+start cmd /c "python main.py --logic RAFLY --email=ya2@example.com --name=raf1 --password=12312423412512341234 --team etimo"
+start cmd /c "python main.py --logic n_ab --email=ya3@example.com --name=al2 --password=12312423412512341234 --team etimo"
+start cmd /c "python main.py --logic RAFLY --email=ya4@example.com --name=raf2 --password=12312423412512341234 --team etimo"
+start cmd /c "python main.py --logic n_ab --email=ya5@example.com --name=al3 --password=12312423412512341234 --team etimo"
+start cmd /c "python main.py --logic n_ab --email=ya6@example.com --name=al4 --password=12312423412512341234 --team etimo"
